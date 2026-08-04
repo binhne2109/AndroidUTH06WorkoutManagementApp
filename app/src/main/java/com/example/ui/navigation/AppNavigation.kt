6bb07a5c06f08d2f9842db852cc4e49b0959cd63
binhne2109 <binhpin2@gmail.com>
2026-08-04 14:04:12 +0700

    fix 1 dòng import bị thiếu đã ổn định có những tính năng cơ bản thêm xóa sửa

--- app/src/main/java/com/example/ui/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/ui/navigation/AppNavigation.kt
@@ -6,6 +6,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.ui.WorkoutScreen
+import com.example.ui.WorkoutViewModel
 
 object Route {
     const val WORKOUT_LIST = "workout_list"
